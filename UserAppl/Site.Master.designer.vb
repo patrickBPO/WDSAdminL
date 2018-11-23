d0e7c87851f6f535dd578800596e29cfd61b4eed
Patrick Castillo <knightiii@hotmail.com>
2018-11-23 15:35:59 -0600

    Commit Include SiteMap and provider string

--- UserAppl/Site.Master.designer.vb
+++ UserAppl/Site.Master.designer.vb
@@ -14,6 +14,15 @@ Option Explicit On
 Partial Public Class SiteMaster
     
     '''<summary>
+    '''SiteMapPath1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents SiteMapPath1 As Global.System.Web.UI.WebControls.SiteMapPath
+    
+    '''<summary>
     '''LIV control.
     '''</summary>
     '''<remarks>
